3ba0ab178780b936ee6b5d5c08685c9a65859b8c
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-11-03 16:25:39 +0530

    Serving static files with express. Added Moment.js.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/vendor'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
